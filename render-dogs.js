154adfabf5443386d5276ca7e52ce0e63b0724a3
THOMAS HARDING <hardingt604@gmail.com>
2022-03-15 13:43:08 -0600

    added data and render function for array property domain

---
+++ render-dogs.js
@@ -0,0 +1,24 @@
+export function renderDogs(dogObject) {
+    let dogDiv = document.createElement('div');
+    dogDiv.classList.add('dog-item');
+
+    let breedDiv = document.createElement('p');
+    breedDiv.textContent = dogObject.breed;
+    dogDiv.appendChild(breedDiv);
+
+    let nameDiv = document.createElement('p');
+    nameDiv.textContent = dogObject.name;
+    dogDiv.appendChild(nameDiv);
+
+    let eyesDiv = document.createElement('p');
+    eyesDiv.textContent = dogObject.eyes;
+    dogDiv.appendChild(eyesDiv);
+
+    let countriesList = document.createElement('ul');
+    for (let country of dogObject.countries) {
+        let countryItem = document.createElement('li');
+        countryItem.textContent = country;
+        countriesList.appendChild(countryItem);
+    }
+    return dogDiv;
+}
